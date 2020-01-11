c69fabbd51bfbb5d3d2d76e486ef6a42395894b4
MukukaCh <chewe.mukuka@gmail.com>
2020-01-11 13:10:21 +0200

    Feature: Helper Utility: Rounding off: Arraylength

---
+++ HelpersTests/ArraylengthTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.Tests
+{
+    [TestClass()]
+    public class ArraylengthTests
+    {
+        private readonly int[] arrayA = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+        [TestMethod()]
+        public void ArrayLengthTest()
+        {
+            Assert.AreEqual(10, Helpers.Arraylength.ArrayLength(arrayA));
+        }
+    }
+}
---
+++ HelpersTests/RoundingTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers.Tests
+{
+    [TestClass()]
+    public class RoundingTests
+    {
+        private readonly double num = 41.759900000005;
+        [TestMethod()]
+        public void RoundTwoDecimalPlacesTest()
+        {
+            Assert.AreEqual(41.76, Helpers.Rounding.RoundTwoDecimalPlaces(num));
+        }
+    }
+}
