962c4ba8fcce3316e8c49232d278ad7fda7456df
MukukaCh <chewe.mukuka@gmail.com>
2020-01-12 22:27:59 +0200

    Feature: Added StatMean Object and IstatMean Interface

---
+++ StatMean/IStatMean.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatMean
+{
+   public  interface IStatMean
+    {
+        public double Meanvalue(double[] ArrayValues);
+    }
+}
---
+++ StatMean/StatMean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using StatisticalCalculatorOperations;
+
+namespace StatMean
+{
+    public class StatMean : IStatMean
+
+    {
+        public dynamic result;
+        public double Meanvalue(double[] ArrayValues)
+        {
+
+            return result = Mean.MeanValue(ArrayValues);
+        }
+    }
+}
