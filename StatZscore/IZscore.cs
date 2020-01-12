e412b7a2325188e2307121f53c1865fb5a42d726
MukukaCh <chewe.mukuka@gmail.com>
2020-01-12 22:32:40 +0200

    Feature: Added Zscore Object and Interface

---
+++ StatZscore/IZscore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatZscore
+{
+  public  interface IZscore
+    {
+        public double ZScore(dynamic score, dynamic values);
+    }
+}
---
+++ StatZscore/StatZscore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StatisticalCalculatorOperations;
+
+namespace StatZscore
+{
+    public class StatZscore : IZscore
+    {
+        public dynamic result;
+
+        public double ZScore(dynamic score, dynamic values)
+        {
+            return result = Zscore.ZScore(score, values);
+        }
+    }
+}
