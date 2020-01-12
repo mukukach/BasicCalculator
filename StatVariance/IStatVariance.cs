30370e82c8ee6b5624d6a383f2362f22b4bfb0b7
MukukaCh <chewe.mukuka@gmail.com>
2020-01-12 22:30:42 +0200

    Feature: Added StatVariance Object and Interface

---
+++ StatVariance/IStatVariance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatVariance
+{
+  public  interface IStatVariance
+    {
+        public double Findvariance(double[] arrayA);
+    }
+}
---
+++ StatVariance/StatVariance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StatisticalCalculatorOperations;
+
+namespace StatVariance
+{
+    public class StatVariance: IStatVariance
+    {
+        public dynamic result;
+
+        public double Findvariance(double[] arrayA)
+        {
+            return result = Variance.Findvariance(arrayA);
+        }
+    }
+}
