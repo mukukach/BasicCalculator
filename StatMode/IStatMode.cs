2f086bec8fa7c656aec346d99dc301f3bb06b888
MukukaCh <chewe.mukuka@gmail.com>
2020-01-13 13:06:22 +0200

    Feature: Mode Object

---
+++ StatMode/IStatMode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatMode
+{
+   public  interface IStatMode
+    {
+        public double Findmode(int[] x);
+    }
+}
---
+++ StatMode/StatMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StatisticalCalculatorOperations;
+
+namespace StatMode
+{
+    public class StatMode: IStatMode
+    {
+        public double result;
+        public double Findmode(int[] x)
+        {
+            return result = Mode.Findmode(x);
+        }
+    }
+}
