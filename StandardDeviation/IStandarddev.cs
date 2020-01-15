70b87973196c71b4f760e333ddde97dbff3c4c6c
MukukaCh <chewe.mukuka@gmail.com>
2020-01-15 13:47:26 +0200

    Feature: Fix Standard Deviation object and interface

---
+++ StandardDeviation/IStandarddev.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Standarddev
+{
+   public interface IStandarddev
+    {
+        public double SD(dynamic value);
+    }
+}
---
+++ StandardDeviation/StandardDev.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StatisticalCalculatorOperations;
+
+namespace Standarddev
+{
+    public class StandardDev : IStandarddev
+    {
+        public double result;
+
+        public double SD(dynamic value)
+        {
+            return result = StandardDeviation.SD(value);
+        }
+    }
+}
